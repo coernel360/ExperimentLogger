c5bb623ef9250ef8e443a34b02e6941ad40260dc
coernel360 <sebastian_w@posteo.de>
2018-11-26 19:32:12 +0100

    First entry
    Added Unity Project

---
+++ ExperimentLogger/Assets/LoggerManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+/// <summary>
+/// This class attached as an Editorscript is used to set up 
+/// PositionLogging in 3D Space for all items in the Array.
+/// 
+/// </summary>
+
+
+public class LoggerManager : MonoBehaviour {
+
+    [Tooltip("An object where the position logging is relative to. Used 0,0,0 if empty.")]
+    [SerializeField] GameObject anchorObject;
+
+    [SerializeField] GameObject[] objectsToLog;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
