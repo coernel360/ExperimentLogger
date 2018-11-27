e94b8fe820158d6a4ab5c4d74d8338a0adbfa0dd
sweiss <sebastian.weiss@offis.de>
2018-11-27 15:23:02 +0100

    Added super-simple position logging

---
+++ ExperimentLogger/Assets/LogWriter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LogWriter : MonoBehaviour
+{
+    [Tooltip ("Set logging frequency here (x/s)")]
+    [SerializeField]
+    float LoggingFrequency;
+
+    [Tooltip ("Set to true if you want to log during next play")]
+    [SerializeField]
+    bool Log;
+
+    StreamWriter writer;
+	// Use this for initialization
+	void Start () {
+        var path = "Assets/Logs/";
+        //check if directory doesn't exit
+        if (!Directory.Exists (path))
+        {
+            //if it doesn't, create it
+            Directory.CreateDirectory (path);
+        }
+        var LogFileName =  ".csv";
+
+        writer = new StreamWriter (path + LogFileName);
+
+        InvokeRepeating ("WriteLog", 0, 1 / LoggingFrequency);
+	}
+
+    void WriteLog()
+    {
+        if (Log)
+            writer.WriteLine ("test" + " , " + DateTime.Now.Millisecond);
+    }
+
+    // Update is called once per frame
+    private void OnApplicationQuit ()
+    {
+        writer.Close ();
+    }
+
+
+}
--- ExperimentLogger/Assets/LoggerManager.cs
+++ ExperimentLogger/Assets/LoggerManager.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
-
+using System.IO;
+using System;
 
 /// <summary>
 /// This class attached as an Editorscript is used to set up 
@@ -16,15 +16,39 @@ public class LoggerManager : MonoBehaviour {
     [Tooltip("An object where the position logging is relative to. Used 0,0,0 if empty.")]
     [SerializeField] GameObject anchorObject;
 
+
+    [Tooltip ("Set logging frequency here (x/s)")]
+    [SerializeField]
+    float LoggingFrequency;
+
     [SerializeField] GameObject[] objectsToLog;
+
+    private StreamWriter writer;
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        var path = "Assets/Logs/";
+        //check if directory doesn't exit
+        if (!Directory.Exists (path))
+        {
+            //if it doesn't, create it
+            Directory.CreateDirectory (path);
+        }
+        var LogFileName = "AllItems.csv";
+
+        writer = new StreamWriter (path + LogFileName);
+
+        InvokeRepeating ("WriteLog", 0, 1 / LoggingFrequency);
+    }
+
+    void WriteLog ()
+    {
+        foreach (GameObject go in objectsToLog)
+            writer.WriteLine (go.name + "," + go.transform.position.ToString () + "," + DateTime.Now.Millisecond);
+    }
+
+    private void OnApplicationQuit ()
+    {
+     writer.Close ();
+    }
 }
